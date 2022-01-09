54da1eb247d4e135f82fa743d39b1d29825f7dd1
Harshil Mavani <hmavani7@gmail.com>
2022-01-09 18:37:38 -0500

    lowest, median, highest vehicle price match added

--- usecases/usecases.go
+++ usecases/usecases.go
@@ -3,6 +3,7 @@ package usecases
 import (
 	"engineering-task/domain"
 	"fmt"
+	"sort"
 	"strings"
 )
 
@@ -13,12 +14,22 @@ type Suggestion struct {
 	Price string
 }
 
+type pricingData struct {
+	vehicleCount int
+	price        int
+}
+
+type PricingStatistic struct {
+	Vehicle      string
+	LowestPrice  int
+	MedianPrice  int
+	HighestPrice int
+}
+
 type SearchResult struct {
 	TotalCount          int
 	MakeModelMatchCount int
-	LowestPrice         int
-	HighestPrice        int
-	MedianPrice         int
+	PricingStatistics   []PricingStatistic
 	Suggestions         []Suggestion
 }
 
@@ -42,24 +53,83 @@ type SearchInteractor struct {
 	Logger        Logger
 }
 
-func (si SearchInteractor) Search(make, model string, year, price int) SearchResult {
+func (si SearchInteractor) Search(manufacturer, model string, year, price int) SearchResult {
 	cars := si.CarRepository.GetAllCars()
 	si.Logger.Log(fmt.Sprintf("Scanning through %d records", len(cars)))
 
 	totalCars := 0
+	makeModelMatch := 0
+	pricingStatisticsCandidates := []string{}
+	pricingDataMap := make(map[string][]pricingData)
+
 	for _, car := range cars {
-		if isCompleteMatch(make, model, year, price, car) {
+
+		var carKey = car.Make + car.Model
+		if data, ok := pricingDataMap[car.Make+car.Model]; ok {
+			data = append(data, pricingData{vehicleCount: car.VehicleCount, price: car.Price})
+			pricingDataMap[carKey] = data
+		} else {
+			pricingDataMap[carKey] = []pricingData{{vehicleCount: car.VehicleCount, price: car.Price}}
+		}
+
+		if isCompleteMatch(manufacturer, model, year, price, car) {
 			totalCars += car.VehicleCount
+		}
+		if isMakeModelMatch(manufacturer, model, car) {
+			makeModelMatch += car.VehicleCount
+		}
+		if price == car.Price {
+			pricingStatisticsCandidates = append(pricingStatisticsCandidates, car.Make+car.Model)
 		}
 	}
 
+	pricingStatistics := computePricingStatistics(pricingStatisticsCandidates, pricingDataMap)
+
 	return SearchResult{
-		TotalCount: totalCars,
+		TotalCount:          totalCars,
+		MakeModelMatchCount: makeModelMatch,
+		PricingStatistics:   pricingStatistics,
 	}
 }
 
 func isCompleteMatch(make, model string, year, price int, car domain.Car) bool {
+	return isMakeModelMatch(make, model, car) && car.Year == year && car.Price == price
+}
+
+func isMakeModelMatch(make, model string, car domain.Car) bool {
 	return strings.HasPrefix(strings.ToLower(car.Make), strings.ToLower(make)) &&
-		strings.HasPrefix(strings.ToLower(car.Model), strings.ToLower(model)) &&
-		car.Year == year && car.Price == price
+		strings.HasPrefix(strings.ToLower(car.Model), strings.ToLower(model))
+}
+
+func computePricingStatistics(pricingStatisticsCandidates []string, pricingDataMap map[string][]pricingData) []PricingStatistic {
+	var pricingStatistics []PricingStatistic
+	for _, carName := range pricingStatisticsCandidates {
+		data := pricingDataMap[carName]
+		sort.Slice(data, func(i, j int) bool {
+			return data[i].price < data[j].price
+		})
+
+		totalVehicles := 0
+		for _, pricingData := range data {
+			totalVehicles += pricingData.vehicleCount
+		}
+		medianVehicleIndex := totalVehicles / 2
+		medianPrice := 0
+		for _, pricingData := range data {
+			medianVehicleIndex = medianVehicleIndex - pricingData.vehicleCount
+			if medianVehicleIndex <= 0 {
+				medianPrice = pricingData.price
+				break
+			}
+		}
+
+		pricing := PricingStatistic{
+			Vehicle:      carName,
+			LowestPrice:  data[0].price,
+			HighestPrice: data[len(data)-1].price,
+			MedianPrice:  medianPrice,
+		}
+		pricingStatistics = append(pricingStatistics, pricing)
+	}
+	return pricingStatistics
 }
--- usecases/usecases_test.go
+++ usecases/usecases_test.go
@@ -10,6 +10,19 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var cars = []domain.Car{
+	{Make: "Tesla", Model: "Model Y", Year: 2019, Price: 50000, VehicleCount: 30},
+	{Make: "Acura", Model: "IDX", Year: 2017, Price: 20000, VehicleCount: 30},
+	{Make: "Honda", Model: "CRV", Year: 2018, Price: 60000, VehicleCount: 50},
+	{Make: "Kia", Model: "EV6", Year: 2019, Price: 50000, VehicleCount: 30},
+	{Make: "Kia", Model: "EV6", Year: 2020, Price: 60000, VehicleCount: 30},
+	{Make: "Ford", Model: "Mach E", Year: 2020, Price: 60000, VehicleCount: 10},
+	{Make: "Tesla", Model: "Model Y", Year: 2018, Price: 40000, VehicleCount: 60},
+	{Make: "Hyundai", Model: "Kona", Year: 2018, Price: 50000, VehicleCount: 60},
+	{Make: "Hyundai", Model: "Kona", Year: 2019, Price: 40000, VehicleCount: 60},
+	{Make: "Hyundai", Model: "Kona", Year: 2019, Price: 70000, VehicleCount: 60},
+}
+
 func instantiateSearchInteractor(cars []domain.Car) usecases.SearchInteractor {
 	carRepository := mocks.CarRepository{Cars: cars}
 	searchInteractor := usecases.SearchInteractor{
@@ -19,26 +32,31 @@ func instantiateSearchInteractor(cars []domain.Car) usecases.SearchInteractor {
 	return searchInteractor
 }
 
+func validateResults(t *testing.T, searchResult usecases.SearchResult, expectedTotalCount, expectedMakeModelMatchCount int, expectedPricingStatistic []usecases.PricingStatistic) {
+	assert.Equal(t, expectedTotalCount, searchResult.TotalCount)
+	assert.Equal(t, expectedMakeModelMatchCount, searchResult.MakeModelMatchCount)
+	assert.Equal(t, expectedPricingStatistic, searchResult.PricingStatistics)
+}
+
 func TestSearch_ComputesTotalCarsFound_SearchResultUpdated(t *testing.T) {
-	cars := []domain.Car{
-		{Make: "Tesla", Model: "Model Y", Year: 2019, Price: 50000, VehicleCount: 30},
-		{Make: "Acura", Model: "IDX", Year: 2017, Price: 20000, VehicleCount: 30},
-		{Make: "Honda", Model: "CRV", Year: 2018, Price: 60000, VehicleCount: 50},
-		{Make: "Kia", Model: "EV6", Year: 2019, Price: 50000, VehicleCount: 30},
-		{Make: "Ford", Model: "Mach E", Year: 2020, Price: 60000, VehicleCount: 10},
-	}
 	searchInteractor := instantiateSearchInteractor(cars)
 
 	searchResult := searchInteractor.Search("Tesla", "Model Y", 2019, 50000)
-	assert.Equal(t, 30, searchResult.TotalCount)
+	expectedPricingStatistics := []usecases.PricingStatistic{
+		{Vehicle: "TeslaModel Y", LowestPrice: 40000, HighestPrice: 50000, MedianPrice: 40000},
+		{Vehicle: "KiaEV6", LowestPrice: 50000, HighestPrice: 60000, MedianPrice: 50000},
+		{Vehicle: "HyundaiKona", LowestPrice: 40000, HighestPrice: 70000, MedianPrice: 50000},
+	}
+	validateResults(t, searchResult, 30, 90, expectedPricingStatistics)
 }
 
 func TestSearch_CaseSensitiveNamesPassed_SearchResultUpdated(t *testing.T) {
-	cars := []domain.Car{
-		{Make: "Tesla", Model: "Model y", Year: 2019, Price: 50000, VehicleCount: 30},
-	}
 	searchInteractor := instantiateSearchInteractor(cars)
 
-	searchResult := searchInteractor.Search("tEsLa", "MoDeL y", 2019, 50000)
-	assert.Equal(t, 30, searchResult.TotalCount)
+	searchResult := searchInteractor.Search("tEsLa", "MoDeL y", 2019, 70000)
+	expectedPricingStatistics := []usecases.PricingStatistic{
+		{Vehicle: "HyundaiKona", LowestPrice: 40000, HighestPrice: 70000, MedianPrice: 50000},
+	}
+
+	validateResults(t, searchResult, 0, 90, expectedPricingStatistics)
 }
